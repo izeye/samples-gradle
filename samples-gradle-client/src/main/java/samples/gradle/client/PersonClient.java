dbbcdcb9cfe993b82196acc7f487b4ea808f7731
izeye <izeye@naver.com>
2015-03-08 12:34:55 +0900

    Initial commit

---
+++ samples-gradle-client/src/main/java/samples/gradle/client/PersonClient.java
@@ -0,0 +1,14 @@
+package samples.gradle.client;
+
+import samples.gradle.common.Person;
+
+/**
+ * Created by izeye on 15. 3. 8..
+ */
+public class PersonClient {
+
+	public static void main(String[] args) {
+		System.out.println(new Person("Johnny", 35));
+	}
+
+}
---
+++ samples-gradle-server/src/main/java/samples/gradle/server/PersonServer.java
@@ -0,0 +1,14 @@
+package samples.gradle.server;
+
+import samples.gradle.common.Person;
+
+/**
+ * Created by izeye on 15. 3. 8..
+ */
+public class PersonServer {
+
+	public static void main(String[] args) {
+		System.out.println(new Person("Johnny", 35));
+	}
+
+}
